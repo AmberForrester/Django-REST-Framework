93187b13c05d36bbd594e26d49be4c4068fdde16
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-15 16:39:27 -0400

    Django Model Instance to Dictionary

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,22 +1,20 @@
 import json
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 
 from products.models import Product
 
 def api_home(request, *args, **kwargs): 
+    # Process of serialization: take a representation of model instance (model_data) -> we want to turn it into a Python Dict -> return JSON to my client
+    # Take a manual approach to the JsonResponse: 
+    # Convert Django Model instance to Dict 
     
     model_data = Product.objects.all().order_by('?').first()
     data = {}
     
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content 
-        data['price'] = model_data.price 
-        # Process of serialization: take a representation of model instance (model_data) -> we want to turn it into a Python Dict -> return JSON to my client
-        
-        # Take a manual approach to the JsonResponse: 
-           
+        data = model_to_dict(model_data, fields=['id', 'title', 'price']) # Clean and easy way to narrow down data from a Model Instance
+    
     return JsonResponse(data)
     
     
@@ -25,17 +23,28 @@ def api_home(request, *args, **kwargs):
     
     
     
+    # return JsonResponse(data): accepts a Dict as an argument
+    
+    #   json_data_str = json.dumps(data)
+    # return HttpResponse(json_data_str, headers={'content-type'}: 'application/json'): accepts a string 
+    # Then error occurs with price, needing to change its serialization ... which is why manually doing things can be tricky, the solution = framework
     
     
+   
+    """ model_data = Product.objects.all().order_by('?').first()
+    data = {}
     
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content 
+        data['price'] = model_data.price 
+        
+        
+    return JsonResponse(data) """
     
-    
-    
-    
-    
-    
-    
-    
+
+
     """ # This request being passed through is an HttpRequest instance from Django.
     # print(dir(request))
     # request.body
